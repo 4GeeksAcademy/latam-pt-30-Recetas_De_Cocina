5ac8aea655f971db2fbc86be713106676a07a54e
Huber Soto <104796746+hubers0@users.noreply.github.com>
2024-07-15 18:30:31 +0000

    cambio en imagenes

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,10 @@ import { Context } from "../store/appContext";
 import recetasImageUrl from "../../img/recetas.jpeg";
 import recetasImageUr2 from "../../img/recetas 2.jpeg";
 import recetasImageUr3 from "../../img/recetas 3.jpg";
+import recetasPostres from "../../img/postres.jpg";
+import recetasDesayunos from "../../img/Desayunos.jpeg";
+import recetasAlmuerzos from "../../img/almuerzos.jpg";
+import recetasKeto from "../../img/Keto.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -16,16 +20,16 @@ export const Home = () => {
              </header>
 			 <div class="row align-items-center">
 			 <div className="col">
-								<img src={recetasImageUr2} style={{maxWidth: "20rem", maxHeight: "20rem"}}/>
+								<img src={recetasImageUr2} className="img-fluid rounded-circle" style={{maxWidth: "20rem", maxHeight: "20rem"}}/>
 							</div>
 			<div className="col">
 			<p>
-				<img src={recetasImageUrl} style={{maxWidth: "20rem", maxHeight: "20rem"}} />
+				<img src={recetasImageUrl} className="img-fluid rounded-circle" style={{maxWidth: "20rem", maxHeight: "20rem"}} />
 			</p>
 			</div>
 			<div className="col">
 			<p>
-				<img src={recetasImageUr3} style={{maxWidth: "20rem", maxHeight: "20rem"}} />
+				<img src={recetasImageUr3} className="img-fluid rounded-circle" style={{maxWidth: "20rem", maxHeight: "20rem"}} />
 			</p>
 			</div>
 			</div>
@@ -35,6 +39,9 @@ export const Home = () => {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Postres</h5>
+            <p>
+				<img src={recetasPostres} className="img-fluid rounded-circle" style={{maxWidth: "17rem", maxHeight: "17rem"}} />
+			</p>
             <p className="card-text">Deliciosos postres para cada ocasión.</p>
             <a href="#" className="btn btn-secondary bg-dark">Ver recetas</a>
           </div>
@@ -44,6 +51,9 @@ export const Home = () => {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Desayunos</h5>
+            <p>
+				<img src={recetasDesayunos} className="img-fluid rounded-circle" style={{maxWidth: "10rem", maxHeight: "10rem"}} />
+			</p>
             <p className="card-text">Comienza tu día con energía.</p>
             <a href="#" className="btn btn-secondary bg-dark ">Ver recetas</a>
           </div>
@@ -53,6 +63,9 @@ export const Home = () => {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Almuerzos</h5>
+            <p>
+				<img src={recetasAlmuerzos} className="img-fluid rounded-circle" style={{maxWidth: "13rem", maxHeight: "13rem"}} />
+			</p>
             <p className="card-text">Recetas para una comida completa.</p>
             <a href="#" className="btn btn-secondary bg-dark ">Ver recetas</a>
           </div>
@@ -62,6 +75,9 @@ export const Home = () => {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Recetas Keto</h5>
+            <p>
+				<img src={recetasKeto} className="img-fluid rounded-circle" style={{maxWidth: "10rem", maxHeight: "10rem"}} />
+			</p>
             <p className="card-text">Recetas bajas en carbohidratos.</p>
             <a href="#" className="btn btn-secondary bg-dark">Ver recetas</a>
           </div>
