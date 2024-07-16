54da8b9c8f6b8eb2cc132b7ad4f825443fa5718e
Huber Soto <104796746+hubers0@users.noreply.github.com>
2024-07-16 03:27:39 +0000

    agregue chilaquiles

--- src/front/js/pages/Chilaquiles.js
+++ src/front/js/pages/Chilaquiles.js
@@ -12,27 +12,24 @@ export const Chilaquiles = () => {
 		<div className="container-fluid   d-flex flex-column justify-content-center" style={{ minHeight: "100vh" }}>
                     <div className="row border-bottom border-2 pb-4 border-danger py-5">
                         <div className="col-md-6">
-                        <img src={chilaquilesverdes} className="img-fluid rounded-circle" style={{maxWidth: "20rem", maxHeight: "20rem"}}/>
+                        <img src={chilaquilesverdes} className="img-fluid  rounded-circle" style={{maxWidth: "50rem", maxHeight: "50rem"}}/>
                         </div>
                         <div className="col-md-6 text-center d-flex flex-column justify-content-center">
-                    <h1>Chilaquiles Verdes</h1>
-                    <p>Ingredientes</p>
-                    <p>
-                        
-    600 Gramos de tomates verdes cocidos
-    1/4 De pieza de cebolla mediana, cocida
-    2 Dientes de ajo cocidos
-    2 Chiles serrano cocidos
-    1 1/2 Tazas de agua
-    5 Ramas de cilantro desinfectado
-    1 1/2 Cucharadas de consomé de pollo en polvo
-    1 Cucharada de aceite vegetal
-    1 Bolsa de totopos (350 g)
-    1 Pechuga de pollo cocida y deshebrada (500 g)
-    1 Envase de Media Crema NESTLÉ® refrigerada (190 g)
-    200 Gramos de queso fresco desmoronado
-
-                    </p>
+                    <h1><strong>Chilaquiles Verdes</strong></h1>
+                    <p><strong>Ingredientes</strong></p>
+                    
+                    <p>600 Gramos de tomates verdes cocidos.</p>
+                    <p>1/4 De pieza de cebolla mediana, cocida.</p>
+                    <p>2 Dientes de ajo cocidos. </p>
+                    <p>2 Chiles serrano cocidos.</p>
+                    <p> 1 1/2 Tazas de agua.</p>
+                    <p> 5 Ramas de cilantro desinfectado. </p>
+                    <p> 1 1/2 Cucharadas de consomé de pollo en polvo. </p>
+                    <p>1 Cucharada de aceite vegetal. </p>
+                    <p>1 Bolsa de totopos (350 g) </p>
+                    <p> 1 Pechuga de pollo cocida y deshebrada (500 g) </p>
+                    <p>1 Envase de Media Crema refrigerada (190 g) </p>
+                    <p>200 Gramos de queso fresco desmoronado. </p>
 
                     <h1><strong>Pasos</strong></h1>
                     <p><strong>Prepara la salsa</strong></p>
