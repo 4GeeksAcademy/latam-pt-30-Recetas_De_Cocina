dec30a4c4fbb9ba04114fe7511344af36652cd58
JurBri <89058157+JurBri@users.noreply.github.com>
2024-07-22 06:11:12 +0000

    se agrega el codigo para crear las tablas en el archivo models, se comenta para revisar si todo esta bien con brian

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,50 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+        
+        
+        
+    """" 
+    Class Categoria(Base)
+    __tablename = "categoria"
+    id = column(integer, primary_key=True)
+    platos = column(string(50))
+
+    Class Plato(Base)
+    __tablename = "plato"
+    id = column(integer, primary_key=True)
+    nombre = column(string(50))
+    
+
+    Class Ingredientes(Base)
+    __tablename = "ingredientes"
+    id = column(integer, primary_key=True)
+    nombre = column(String(250), nullable=False)
+    cantidad = column(Float, nullable=False)
+
+     Class Pasos(Base)
+    __tablename = "pasos"
+    id = column(integer, primary_key=True)
+    description = column(string(50))
+
+     Class InformacionNutritiva(Base)
+    __tablename = "informacion_nutritiva"
+    id = column(integer, primary_key=True)
+    calorias = column(Float, nullable=False)
+    carbohidratos = column(Float, nullable=False)
+    energia = column(Float)
+    grasa = column(Float)
+    proteina = column(Float)
+    fibra = column(Float)
+    azucares = column(Float, nullable=False)
+    grasas saturadas = column(Float)
+    sodio = column(Float)
+
+    
+
+
+    
+    
+    
+    """
