1a504790dfa29cd17e608376f3b23bae776b5b14
Huber Soto <104796746+hubers0@users.noreply.github.com>
2024-07-25 23:37:15 +0000

    agregue login

--- src/front/js/component/signup.js
+++ src/front/js/component/signup.js
@@ -2,17 +2,36 @@ import React from "react";
 
 export const Registro = () => {
     return (
-        <form id="formsign" class="col-3">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label"><strong>Correo electrónico</strong></label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">Nunca compartiremos su correo electrónico con nadie más.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label"><strong>Contraseña</strong></label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
-    </div>
-  <button type="submit" class="btn btn-primary bg-dark">Enviar</button>
-</form>
+      <React.Fragment>
+      <div className="containerLogin">
+          <div className="h2Login">
+              <h2> LOGIN </h2>
+          </div>
+          <form className="formLogin" onSubmit={handleSubmit}>
+              <div className="col-12 row">
+                  <div>
+                      <label>Email:</label>
+                      <input 
+                      type="email"
+                      value={email}
+                      onChange={(event) => setEmail(event.target.value)}
+                      />
+                  </div>
+
+                  <div>
+                      <label>Password:</label>
+                      <input 
+                      type="password"
+                      value={password}
+                      onChange={(event) => setPassword(event.target.value)}
+                      />
+                  </div>
+              </div>
+              <div className="buttonLogin">
+                  <button type="submit"> Login </button>
+              </div>
+          </form>    
+      </div>   
+       </React.Fragment>
     )
 }
